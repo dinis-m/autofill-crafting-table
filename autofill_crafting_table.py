cc18ce047bf456db6db2679a3630627a7fdd82a2
dinis-m <dinis.mangas@gmail.com>
2026-06-03 16:36:03 +0100

    Add files via upload

---
+++ autofill_crafting_table.py
@@ -0,0 +1,14 @@
+import minescript as m
+from recipe_helper import get_recipes_id
+from java import JavaClass
+
+Minecraft = JavaClass("net.minecraft.client.Minecraft")
+RecipeDisplayId = JavaClass("net.minecraft.world.item.crafting.display.RecipeDisplayId")
+ServerboundPlaceRecipePacket = JavaClass("net.minecraft.network.protocol.game.ServerboundPlaceRecipePacket")
+mc = Minecraft.getInstance() # type: ignore
+
+def autofill_craft_item(item_name: str):
+    try:
+        mc.getConnection().send(ServerboundPlaceRecipePacket(mc.player.containerMenu.containerId, RecipeDisplayId(get_recipes_id(item_name)), True)) # type: ignore
+    except Exception as e:
+        m.echo(f"Error: {e}")
